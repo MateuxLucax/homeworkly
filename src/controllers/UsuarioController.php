9cd7d1561d4d6d4eea78029ac51000262985d3a0
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-11-21 15:28:00 -0300

    fix: StatementBuilder -> Query

--- src/controllers/UsuarioController.php
+++ src/controllers/UsuarioController.php
@@ -2,8 +2,8 @@
 
 $root = $_SERVER['DOCUMENT_ROOT'] . "/../";
 
-require_once $root ."models/Usuario.php";
-require_once $root . "database/StatementBuilder.php";
+require_once $root . "models/Usuario.php";
+require_once $root . "database/Query.php";
 require_once $root . "utils/PasswordUtil.php";
 require_once $root . "exceptions/UnauthorizedException.php";
 
