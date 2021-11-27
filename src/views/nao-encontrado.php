024f71cd3b08973880c946fbe15cd7028e9baf3d
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-11-26 21:02:32 -0300

    feat: view para recurso não encontrado

---
+++ src/views/nao-encontrado.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once $root.'views/componentes/head.php' ?>
+
+<body>
+
+    <main class="container">
+        <div class="alert alert-danger text-center mt-5" role="alert">
+            <h4 class="alert-heading">404 - Não encontrado</h4>
+            <hr>
+            <p class="mb-0"><?= $view['mensagem'] ?></p>
+        </div>
+    </main>
+    
+</body>
+
+</html>
