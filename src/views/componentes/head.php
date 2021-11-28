a0799166aaff3a121cc9dd1fba57957a4bcc6441
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-11-28 18:10:57 -0300

    fix: melhor comportamento qdo $view['title'] não é informado

--- src/views/componentes/head.php
+++ src/views/componentes/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HomeWorkly - <?=$view['title']?></title>
+    <title>HomeWorkly <?= isset($view['title']) ? " - {$view['title']}" : '' ?></title>
     <link rel="shortcut icon" href="/static/images/favicon.svg" type="image/svg+xml" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
