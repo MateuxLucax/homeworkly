7d7d00aef30302af06452b495685a0fe98622445
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-11-29 23:13:11 -0300

    feat: página de criação de turmas com criação de disciplinas junto

--- src/public/admin/turmas/criar.php
+++ src/public/admin/turmas/criar.php
@@ -9,23 +9,52 @@ require_once $root.'models/TipoUsuario.php';
 
 UsuarioController::validaSessaoTipo(TipoUsuario::ADMINISTRADOR);
 
+require_once $root.'database/Connection.php';
 require_once $root.'database/Query.php';
 require_once $root.'utils/HttpCodes.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    $data = readRequestBody();
-    $ok = Query::execute('INSERT INTO turma (nome, ano) VALUES (:nome, :ano)', [
-        ':nome' => $data['nome'],
-        ':ano'  => $data['ano']
-    ]);
-    respond($ok ? HttpCodes::CREATED : HttpCodes::BAD_REQUEST);
-}
-else if ($_SERVER['REQUEST_METHOD'] == 'GET')
+$pdo = Connection::getInstance();
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
     $view['title'] = 'Criar turma';
     require_once $root.'views/turmas/criar.php';
     die();
+}
+else if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $data = readRequestBody();
+
+    try {
+        $pdo->beginTransaction();
+
+        $pdo->prepare('INSERT INTO turma (nome, ano) VALUES (:nome, :ano)')->execute([
+            ':nome' => $data['nome'],
+            ':ano'  => $data['ano']
+        ]);
+
+        $idTurma = $pdo->lastInsertId();
+
+        $disciplinas = $data['disciplinas'];
+
+        $sqlCriarDisciplinas =
+        'INSERT INTO disciplina (nome, id_turma)
+         VALUES '. join(',', array_fill(0, count($disciplinas), '(?, ?)'));
+
+        $paramsCriarDisciplinas = [];
+        foreach ($disciplinas as $disciplina) {
+            $paramsCriarDisciplinas[] = $disciplina;
+            $paramsCriarDisciplinas[] = $idTurma;
+        }
+        
+        $pdo->prepare($sqlCriarDisciplinas)->execute($paramsCriarDisciplinas);
+
+        $pdo->commit();
+        respond(HttpCodes::CREATED, ['id' => $idTurma]);
+    } catch(Exception $e) {
+        $pdo->rollBack();
+        respond(HttpCodes::BAD_REQUEST, ['exception' => $e]);
+    }
 }
 else
 {
--- src/views/turmas/criar.php
+++ src/views/turmas/criar.php
@@ -22,9 +22,22 @@
                 </div>
             </div>
 
+            <div class="card mb-3">
+                <div class="card-header">
+                    Disciplinas
+                </div>
+                <div class="card-body" id="disciplinas-container">
+
+                    <button type="button" style="width: 100%" id="btn-adicionar-disciplina" class="btn btn-outline-success">
+                        <i class="fas fa-plus-circle"></i>
+                    </button>
+
+                </div>
+            </div>
+
             <div class="text-end">
-                <button type="submit" class="btn btn-success btn-lg">
-                    <i class="fas fa-plus-circle"></i>&nbsp;
+                <button type="submit" class="btn btn-outline-primary btn-lg">
+                    <i class="fas fa-paper-plane"></i>&nbsp;
                     Criar
                 </button>
             </div>
@@ -42,26 +55,73 @@
 
             const payload = {
                 nome: form['turma-nome'].value,
-                ano:  form['turma-ano'].value
-            }
+                ano:  form['turma-ano'].value,
+                disciplinas: Array.from(form['disciplina-nome[]']).map(i => i.value)
+            };
+
+            console.log(payload);
 
             fetch('criar', {method: 'POST', body: JSON.stringify(payload)})
             .then(response => {
-                response.text().then(console.log);
                 if (response.status == 201) {
                     agendarAlertaSwal({
                         icon: 'success',
                         text: 'Turma criada com sucesso'
-                    })
-                    window.location.assign('listar');
+                    });
+                    response.json().then(ret => {
+                        window.location.assign(`turma?id=${ret.id}`);
+                    });
                 } else {
                     Swal.fire({
                         icon: 'error',
                         text: 'Não foi possível criar a turma'
                     });
+                    response.text().then(console.log);
                 }
             });
         };
+
+        //
+        // Adicionar disciplinas dinamicamente
+        //
+
+        const disciplinasContainer   = document.getElementById('disciplinas-container');
+        const btnAdicionarDisciplina = document.getElementById('btn-adicionar-disciplina');
+
+        btnAdicionarDisciplina.onclick = () => {
+            disciplinasContainer.insertBefore(
+                criarInputDisciplina(),
+                btnAdicionarDisciplina
+            );
+        };
+
+        function criarInputDisciplina() {
+            const group = document.createElement('div');
+            group.classList.add('mb-3', 'input-group');
+
+            const input = document.createElement('input');
+            Object.assign(input, {
+                type: 'text',
+                name: 'disciplina-nome[]'
+            })
+            input.classList.add('disciplina-nome', 'form-control');
+            group.appendChild(input);
+
+            const iconeRemover = document.createElement('i');
+            iconeRemover.classList.add('fas', 'fa-minus-circle');
+
+            const btnRemover = document.createElement('button');
+            Object.assign(btnRemover, {
+                type: 'button',
+                tabIndex: -1,
+                onclick: () => { group.remove() }
+            });
+            btnRemover.classList.add('btn', 'btn-outline-danger')
+            btnRemover.appendChild(iconeRemover);
+            group.appendChild(btnRemover);
+
+            return group;
+        }
     </script>
 
 </body>
