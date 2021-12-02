5ea853a6d4b5408a3d904314cd7bbabad039170c
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-12-01 22:54:48 -0300

    mudei de ideia, meio chato remover todos os usos + o select não fica tão bom em PDO

--- src/database/Query.php
+++ src/database/Query.php
@@ -3,14 +3,6 @@
 require_once $root.'/database/Connection.php';
 require_once $root.'/exceptions/QueryException.php';
 
-
-// TODO remover essa classe, o próprio PDO já fica bonito o suficiente:
-//
-// $pdo->prepare('INSERT INTO turma (nome, ano) VALUES (:nome, :ano)')->execute([
-//     ':nome' => $data['nome'],
-//     ':ano'  => $data['ano']
-// ]);
-
 class Query
 {
     public static function select(string $sql, array $params = []) : array | false
