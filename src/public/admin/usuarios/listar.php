80948074e5c662e3a80e7705820c07cb65a0c0db
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-12-02 18:15:53 -0300

    feat: usuarios/listar.php pode ler e retornar json + filtrar usuários conforme filtros fornecidos
    mas ainda faltam outros filtros

--- src/public/admin/usuarios/listar.php
+++ src/public/admin/usuarios/listar.php
@@ -4,10 +4,49 @@ $root = '../../../';
 
 require_once $root . 'controllers/UsuarioController.php';
 require_once $root . 'models/TipoUsuario.php';
+require_once $root . 'utils/response-utils.php';
 
 UsuarioController::validaSessaoTipo(TipoUsuario::ADMINISTRADOR);
 
-$view['title'] = 'Usuários';
-$view['usuarios'] = UsuarioController::listarTodos();
+$headers = getallheaders();
 
-require $root.'/views/usuarios/listar.php';
+if ($headers['Accept'] == 'application/json')
+{
+    $data = readJsonRequestBody();
+
+    $condicoes = [];
+    $params = [];
+
+    $filtros = $data['filtros'] ?? [];
+
+    foreach ($filtros as $coluna => $valor) {
+        switch ($coluna) {
+        case 'nome':
+            $condicoes[] = 'LOWER(nome) like :nome';
+            $params[':nome'] = '%'.strtolower($valor).'%';
+            break;
+        case 'tipo':
+            $condicoes[] = 'tipo = :tipo';
+            $params[':tipo'] = $valor;
+            break;
+        }
+    }
+
+    $sql = 'SELECT id_usuario, nome, login FROM usuario';
+
+    if (count($condicoes) > 0) {
+        $sql .= ' WHERE ';
+        $sql .= join(' AND ', $condicoes);
+    }
+
+    $usuarios = Query::select($sql, $params);
+
+    respondJson(HttpCodes::OK, $usuarios);
+}
+else
+{
+    $view['title'] = 'Usuários';
+    $view['usuarios'] = UsuarioController::listarTodos();
+
+    require $root.'/views/usuarios/listar.php';
+}
