b11c3582d04673ea34d9a973c2955f8dff263135
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-12-09 20:21:25 -0300

    fix: removido turmas/editar.php (alterar.php será o correto)

--- src/views/turmas/listar.php
+++ src/views/turmas/listar.php
@@ -35,7 +35,8 @@
                         <td><?=$turma['ano']?></td>
                         <td style="width: 0;">
                             <div class="d-flex justify-content-end">
-                                <button type="button"
+                                <!-- TODO Mudar para um link para /admin/turmas/alterar?id quando essa página existir -->
+                                <!-- <button type="button"
                                         class="btn btn-primary btn-editar-turma me-4"
                                         title="Editar turma"
                                         data-id="<?=$turma['id']?>"
@@ -43,7 +44,7 @@
                                         data-ano="<?=$turma['ano']?>"
                                 >
                                     <i class="fas fa-edit"></i>
-                                </button>
+                                </button> -->
                                 <button type="button"
                                         class="btn btn-danger btn-excluir-turma"
                                         title="Remover turma"
@@ -73,37 +74,6 @@
         </table>
 
     </main>
-
-    <div class="modal fade" id="modal-editar-turma">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Editar turma</h5>
-                </div>
-
-                <form id="form-editar-turma">
-
-                    <input type="hidden" name="id">
-
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label for="nome" class="form-label">Nome</label>
-                            <input type="text" name="nome" id="nome" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="ano" class="form-label">Ano</label>
-                            <input type="text" name="ano" id="ano" class="form-control">
-                        </div>
-                    </div>
-                    
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-success">Editar</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
 
     <div class="modal fade" id="modal-excluir-turma">
         <div class="modal-dialog">
@@ -165,54 +135,6 @@
             modalExcluir.hide();
             inputIdExcluir.value = null;
         });
-
-        //
-        // Editar turma
-        //
-
-        const formEditar = document.getElementById('form-editar-turma');
-        const modalEditar = new bootstrap.Modal(document.getElementById('modal-editar-turma'));
-
-        const inputIdEditar   = formEditar.querySelector('[name=id]');
-        const inputNomeEditar = formEditar.querySelector('[name=nome]');
-        const inputAnoEditar  = formEditar.querySelector('[name=ano]');
-
-        for (const btnEditar of document.querySelectorAll('.btn-editar-turma')) {
-            btnEditar.addEventListener('click', () => {
-                console.log('ok');
-                inputIdEditar.value   = btnEditar.dataset.id;
-                inputNomeEditar.value = btnEditar.dataset.nome;
-                inputAnoEditar.value  = btnEditar.dataset.ano;
-                modalEditar.show();
-            });
-        }
-
-        formEditar.addEventListener('submit', event => {
-            event.preventDefault();
-            const payload = {
-                id:   formEditar.id.value,
-                nome: formEditar.nome.value,
-                ano:  formEditar.ano.value
-            };
-            fetch('editar', { method: 'POST', body: JSON.stringify(payload) })
-            .then(response => {
-                if (response.status == 200) {
-                    agendarAlertaSwal({
-                        icon: 'success',
-                        text: 'Turma alterada com sucesso'
-                    });
-                    window.location.reload();
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        text: 'Não foi possível alterar a turma'
-                    });
-                }
-                response.text().then(console.log);
-            });
-            modalEditar.hide();
-        });
-
     </script>
 
 
