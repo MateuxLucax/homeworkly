a9df782877b3289ab04d74754147859d0e5fc046
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-12-08 23:06:29 -0300

    feat: back-end para a alteração de turmas (incluindo alunos e disciplinas)

---
+++ src/public/admin/turmas/alterar.php
@@ -0,0 +1,74 @@
+<?php
+
+$root = '../../..';
+
+require_once $root.'/utils/response-utils.php';
+
+forbidMethodsNot('POST');
+
+require_once $root.'/controllers/UsuarioController.php';
+require_once $root.'/models/TipoUsuario.php';
+
+UsuarioController::validaSessaoTipo(TipoUsuario::ADMINISTRADOR);
+
+require_once $root.'/database/Connection.php';
+
+$id   = $_GET['id'];
+$data = readJsonRequestBody();
+
+$pdo = Connection::getInstance();
+
+try {
+    $pdo->beginTransaction();
+
+    $pdo->prepare('UPDATE turma SET nome = :nome, ano = :ano WHERE id_turma = :id')->execute([
+          ':nome' => $data['nome']
+        , ':ano'  => $data['ano']
+        , ':id'   => $id
+    ]);
+
+    //
+    // Atualizar alunos
+    //
+
+    $pdo->prepare('DELETE FROM aluno_em_turma WHERE id_turma = :id')->execute([':id' => $id]);
+
+    foreach ($data['alunos'] as $idAluno) {
+        $pdo->prepare('INSERT INTO aluno_em_turma (id_aluno, id_turma) VALUES (:idAluno, :idTurma)')->execute([
+              ':idAluno' => $idAluno
+            , ':idTurma' => $id
+        ]);
+    }
+
+    //
+    // Atualizar disciplinas
+    //
+
+    $pdo->prepare(
+        'DELETE FROM professor_de_disciplina pd USING disciplina d
+         WHERE pd.id_disciplina = d.id_disciplina AND d.id_turma = :id'
+    )->execute([':id' => $id]);
+    $pdo->prepare('DELETE FROM disciplina WHERE id_turma = :id')->execute([':id' => $id]);
+
+    foreach ($data['disciplinas'] as $disciplina) {
+        $pdo->prepare('INSERT INTO disciplina (nome, id_turma) VALUES (:nome, :id)')->execute([
+              ':nome' => $disciplina['nome']
+            , ':id'   => $id
+        ]);
+        $idDisciplina = $pdo->lastInsertId();
+        foreach ($disciplina['professores'] as $idProfessor) {
+            $pdo->prepare(
+                'INSERT INTO professor_de_disciplina (id_professor, id_disciplina) VALUES (:idProf, :idDisc)'
+            )->execute([
+                  ':idProf'  => $idProfessor
+                , ':idDisc' => $idDisciplina
+            ]);
+        }
+    }
+
+    $pdo->commit();
+    respondJson(HttpCodes::OK);
+} catch (Exception $e) {
+    $pdo->rollBack();
+    respondJson(HttpCodes::BAD_REQUEST, ['exception' => $e]);
+}
