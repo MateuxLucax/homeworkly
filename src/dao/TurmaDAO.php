f89c1d51e34a77e49d3d17d515e8e4c23d481dd4
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-12-10 18:49:19 -0300

    refac: refeito back-end da alteração de turma para usar o TurmaDAO

--- src/dao/TurmaDAO.php
+++ src/dao/TurmaDAO.php
@@ -5,7 +5,7 @@ require_once $root.'/database/Connection.php';
 require_once $root.'/database/Query.php';
 require_once $root.'/dao/DisciplinaDAO.php';
 
-class TurmaDao
+class TurmaDAO
 {
     public static function buscarTodas(): array
     {
@@ -43,14 +43,38 @@ class TurmaDao
 
     public static function popularComAlunos(Turma $turma): Turma
     {
-        $turma->setAlunos(UsuarioDAO::buscarAlunosDeTurma($turma->getId()));
-        return $turma;
+        return $turma->setAlunos(UsuarioDAO::buscarAlunosDeTurma($turma->getId()));
     }
 
     public static function popularComDisciplinas(Turma $turma): Turma
     {
         $turma->setDisciplinas(DisciplinaDAO::buscarDeTurma($turma->getId()));
-        foreach ($turma->getDisciplinas() as $d) $d->setTurma($turma);
+        foreach ($turma->getDisciplinas() as $d) {
+            $d->setTurma($turma);
+        }
+        return $turma;
+    }
+
+    public static function associarAlunos(Turma $turma): Turma
+    {
+        $pdo = Connection::getInstance();
+
+        foreach ($turma->getAlunos() as $aluno) {
+            $pdo->prepare('INSERT INTO aluno_em_turma (id_aluno, id_turma) VALUES (:idAluno, :idTurma)')->execute([
+                ':idAluno' => $aluno->getId(),
+                ':idTurma' => $turma->getId()
+            ]);
+        }
+
+        return $turma;
+    }
+
+    public static function criarDisciplinas(Turma $turma): Turma
+    {
+        foreach ($turma->getDisciplinas() as $disciplina) {
+            DisciplinaDAO::criar($disciplina);
+        }
+
         return $turma;
     }
 
@@ -63,16 +87,59 @@ class TurmaDao
         ]);
         $turma->setId($pdo->lastInsertId());
 
-        foreach ($turma->getDisciplinas() as $disciplina) {
-            DisciplinaDAO::criar($disciplina);
-        }
+        self::associarAlunos($turma);
+        self::criarDisciplinas($turma);
 
-        foreach ($turma->getAlunos() as $aluno) {
-            $pdo->prepare('INSERT INTO aluno_em_turma (id_aluno, id_turma) VALUES (:idAluno, :idTurma)')->execute([
-                ':idAluno' => $aluno->getId(),
-                ':idTurma' => $turma->getId()
-            ]);
-        }
+        return $turma;
+    }
+
+    public static function removerAlunos(Turma $turma): Turma
+    {
+        $pdo = Connection::getInstance();
+        $pdo->prepare('DELETE FROM aluno_em_turma WHERE id_turma = :id')->execute([':id' => $turma->getId()]);
+        return $turma;
+    }
+
+    public static function excluirDisciplinas(Turma $turma)
+    {
+        $pdo = Connection::getInstance();
+
+        $pdo->prepare(
+            'DELETE FROM professor_de_disciplina pd USING disciplina d
+             WHERE pd.id_disciplina = d.id_disciplina AND d.id_turma = :id'
+        )->execute([':id' => $turma->getId()]);
+
+        $pdo->prepare('DELETE FROM disciplina WHERE id_turma = :id')->execute([':id' => $turma->getId()]);
+
+        return $turma;
+    }
+
+    public static function excluir(Turma $turma): Turma
+    {
+        $pdo = Connection::getInstance();
+
+        self::removerAlunos($turma);
+        self::excluirDisciplinas($turma);
+        $pdo->prepare('DELETE FROM turma WHERE id_turma = :id')->execute([':id' => $turma->getId()]);
+
+        return $turma;
+    }
+
+    public static function alterar(Turma $turma): Turma
+    {
+        $pdo = Connection::getInstance();
+
+        $pdo->prepare('UPDATE turma SET nome = :nome, ano = :ano WHERE id_turma = :id')->execute([
+            ':id'   => $turma->getId(),
+            ':nome' => $turma->getNome(),
+            ':ano'  => $turma->getAno()
+        ]);
+
+        self::removerAlunos($turma);
+        self::excluirDisciplinas($turma);
+
+        self::associarAlunos($turma);
+        self::criarDisciplinas($turma);
 
         return $turma;
     }
--- src/public/admin/turmas/alterar.php
+++ src/public/admin/turmas/alterar.php
@@ -3,73 +3,53 @@
 $root = '../../..';
 
 require_once $root.'/utils/response-utils.php';
+require_once $root.'/dao/TurmaDAO.php';
 require_once $root.'/dao/UsuarioDAO.php';
+require_once $root.'/models/Turma.php';
 require_once $root.'/models/TipoUsuario.php';
 
 UsuarioDAO::validaSessaoTipo(TipoUsuario::ADMINISTRADOR);
 
 require_once $root.'/database/Connection.php';
 
-$id   = $_GET['id'];
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
-    // TODO
+    $turma = TurmaDAO::buscar($_GET['id']);
+    TurmaDAO::popularComAlunos($turma);
+    TurmaDAO::popularComDisciplinas($turma);
+    $view['turma'] = $turma;
+    $view['title'] = 'Alterar turma';
+    require_once $root.'/views/turmas/criar.php';
 }
 else if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $data = readJsonRequestBody();
 
+    $turma = new Turma;
+    $turma
+        ->setId($data['id'])
+        ->setNome($data['nome'])
+        ->setAno($data['ano'])
+        ->setDisciplinas(array_map(
+            fn($disc) => (new Disciplina)
+                ->setTurma($turma)
+                ->setNome($disc['nome'])
+                ->setProfessores(array_map(
+                    fn($idProf) => (new Usuario)->setId($idProf),
+                    $disc['professores']
+                )),
+            $data['disciplinas']
+        ))
+        ->setAlunos(array_map(
+            fn($idAluno) => (new Usuario)->setId($idAluno),
+            $data['alunos']
+        ));
+
     $pdo = Connection::getInstance();
 
+    $pdo->beginTransaction();
     try {
-        $pdo->beginTransaction();
-
-        $pdo->prepare('UPDATE turma SET nome = :nome, ano = :ano WHERE id_turma = :id')->execute([
-            ':nome' => $data['nome']
-            , ':ano'  => $data['ano']
-            , ':id'   => $id
-        ]);
-
-        //
-        // Atualizar alunos
-        //
-
-        $pdo->prepare('DELETE FROM aluno_em_turma WHERE id_turma = :id')->execute([':id' => $id]);
-
-        foreach ($data['alunos'] as $idAluno) {
-            $pdo->prepare('INSERT INTO aluno_em_turma (id_aluno, id_turma) VALUES (:idAluno, :idTurma)')->execute([
-                ':idAluno' => $idAluno
-                , ':idTurma' => $id
-            ]);
-        }
-
-        //
-        // Atualizar disciplinas
-        //
-
-        $pdo->prepare(
-            'DELETE FROM professor_de_disciplina pd USING disciplina d
-            WHERE pd.id_disciplina = d.id_disciplina AND d.id_turma = :id'
-        )->execute([':id' => $id]);
-        $pdo->prepare('DELETE FROM disciplina WHERE id_turma = :id')->execute([':id' => $id]);
-
-        foreach ($data['disciplinas'] as $disciplina) {
-            $pdo->prepare('INSERT INTO disciplina (nome, id_turma) VALUES (:nome, :id)')->execute([
-                ':nome' => $disciplina['nome']
-                , ':id'   => $id
-            ]);
-            $idDisciplina = $pdo->lastInsertId();
-            foreach ($disciplina['professores'] as $idProfessor) {
-                $pdo->prepare(
-                    'INSERT INTO professor_de_disciplina (id_professor, id_disciplina) VALUES (:idProf, :idDisc)'
-                )->execute([
-                    ':idProf'  => $idProfessor
-                    , ':idDisc' => $idDisciplina
-                ]);
-            }
-        }
-
+        TurmaDAO::alterar($turma);
         $pdo->commit();
         respondJson(HttpCodes::OK);
     } catch (Exception $e) {
