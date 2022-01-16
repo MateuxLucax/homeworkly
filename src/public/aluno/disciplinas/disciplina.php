b87af5524883978377ba2166c263e4b1d746aa60
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2022-01-16 19:52:02 -0300

    (em progresso) tela para aluno visualizar disciplina
    mas é melhor antes desenvolver a visão do aluno de uma tarefa, depois como é feita a entrega etc. pra depois colocar isso no contexto da disciplina
    por isso foi deixado em progresso

---
+++ src/public/aluno/disciplinas/disciplina.php
@@ -0,0 +1,81 @@
+<?php
+
+
+
+// Em progresso
+
+$root = '../../..';
+
+require_once $root . '/utils/response-utils.php';
+forbidMethodsNot('GET');
+require_once $root . '/dao/UsuarioDAO.php';
+require_once $root . '/models/TipoUsuario.php';
+UsuarioDAO::validaSessaoTipo(TipoUsuario::ALUNO);
+
+require_once $root . '/database/DisciplinaDAO.php';
+require_once $root . '/database/TarefaDAO.php';
+require_once $root . '/database/Connection.php';
+
+if (empty($_GET['id'])) {
+    respondWithNotFoundPage('<b>Erro do sistema:</b> A página de disciplina foi acessada sem nenhum ID fornecido.');
+}
+
+$id = $_GET['id'];
+
+$disciplina = DisciplinaDAO::buscar($id);
+
+if ($disciplina == null) {
+    respondWithNotFoundPage('Não existe disciplina com ID <b>'.$id.'</b>');
+}
+
+DisciplinaDAO::popularComProfessores($disciplina);
+
+// Indexa os professores pelos seus IDs para facilitar a associação com as tarefas abaixo
+
+$professoresPorId = [];
+foreach ($disciplina->getProfessores() as $professor) {
+    $professoresPorId[ $professor->getId() ] = $professor;
+}
+
+// TODO enum para situação da tarefa em relação ao aluno
+// TODO mais amplamente, um objeto pra encapsular essa categorização e ordenação das tarefas em relação ao aluno, TarefasPorSituacaoAluno?
+$tarefasPorSituacao = [
+    'atrasadas' => [],
+    'pendentes' => [],
+    'entregues' => []
+];
+
+$sql =
+'SELECT t.id_tarefa
+      , t.id_professor
+      , t.titulo
+      , t.descricao
+      , t.esforco_minutos
+      , t.com_nota
+      , t.abertura
+      , t.entrega
+      , t.fechamento
+      , t.fechada 
+   FROM tarefa t
+  WHERE t.id_disciplina = :id';
+
+$pdo = Connection::getInstance();
+
+$stmt = $pdo->prepare($sql, [':id' => $disciplina->getId()]);
+
+while ($t = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $tarefa = (new Tarefa)
+        ->setId($t['id_tarefa'])
+        ->setTitulo($t['titulo'])
+        ->setDescricao($t['descricao'])
+        ->setEsforcoMinutos($t['esforco_minutos'])
+        ->setAbertura(DateUtil::toLocalDateTime($t['abertura']))
+        ->setEntrega(DateUtil::toLocalDateTime($t['entrega']))
+        ->setFechamento($ta['fechamento'] ? DateUtil::toLocalDateTime($ta['fechamento']) : null)
+        ->setFechadaManualmente($t['fechada'])
+        ->setProfessor($professoresPorId[$t['id_professor']]);
+
+    // Buscar entrega do aluno
+    // Gerar valor em enum da situação aluno-tarefa
+    // Adicionar no $tarefasPorSituacao
+}
