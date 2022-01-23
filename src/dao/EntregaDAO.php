1aa30bd3d3645beb11b4133ec273e7ed81c404e2
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-01-23 15:53:58 -0300

    feat: Entrega, EntregaDAO

---
+++ src/dao/EntregaDAO.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once $root . '/database/Query.php';
+
+require_once $root . '/models/Tarefa.php';
+require_once $root . '/models/Usuario.php';
+require_once $root . '/models/Entrega.php';
+
+
+class EntregaDAO
+{
+    public static function buscar(int $idAluno, int $idTarefa): ?Entrega
+    {
+        $sql =
+        'SELECT conteudo
+              , data_hora
+              , em_definitivo
+              , visto
+              , nota
+              , comentario
+           FROM entrega
+          WHERE (id_aluno, id_tarefa)
+              = (:idAluno, :idTarefa)';
+
+        $params = [
+            ':idAluno' => $idAluno,
+            ':idTarefa' => $idTarefa,
+        ];
+
+        $result = Query::select($sql, $params);
+        
+        if (count($result) == 0) {
+            return null;
+        }
+
+        $e = $result[0];
+
+        return (new Entrega)
+            ->setTarefa((new Tarefa)->setId($idTarefa))
+            ->setAluno((new Usuario)->setId($idAluno))
+            ->setConteudo($e['conteudo'])
+            ->setDataHora(DateUtil::toLocalDateTime($e['data_hora']))
+            ->setEmDefinitivo($e['em_definitivo'])
+            ->setVisto($e['visto'])
+            ->setNota($e['nota'])
+            ->setComentario($e['comentario']);
+    }
+
+    public static function alterar(Entrega $entrega): bool
+    {
+        $sql =
+        'UPDATE entrega
+            SET conteudo = :conteudo
+              , data_hora = :dataHora
+         WHERE (id_aluno, id_tarefa) = (:idAluno, :idTarefa)';
+
+        $params = [
+            ':idAluno'  => $entrega->aluno()->getId(),
+            ':idTarefa' => $entrega->tarefa()->id(),
+            ':conteudo' => $entrega->conteudo(),
+            ':dataHora' => $entrega->dataHora()->format('Y-m-d H:i:s')
+        ];
+
+        return Query::execute($sql, $params);
+    }
+
+    public static function criar(Entrega $entrega): bool
+    {
+        $sql =
+        'INSERT INTO entrega (id_tarefa, id_aluno, conteudo, data_hora, em_definitivo)
+         VALUES (:idTarefa, :idAluno, :conteudo, :dataHora, :emDefinitivo)';
+
+        $params = [
+            ':idTarefa'     => $entrega->tarefa()->id(),
+            ':idAluno'      => $entrega->aluno()->getId(),
+            ':conteudo'     => $entrega->conteudo(),
+            ':dataHora'     => $entrega->dataHora()->format('Y-m-d H:i:s'),
+            ':emDefinitivo' => $entrega->emDefinitivo() ? 'true' : 'false',
+        ];
+
+        return Query::execute($sql, $params);
+    }
+}
